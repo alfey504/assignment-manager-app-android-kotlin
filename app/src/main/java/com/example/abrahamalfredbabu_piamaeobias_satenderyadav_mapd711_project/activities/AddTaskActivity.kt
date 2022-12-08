2a1234b5fa169f3f4910a0886cf338ae49e543c0
alfey504 <alfey504@gmail.com>
2022-12-07 23:54:02 -0500

    added task add activity

---
+++ app/src/main/java/com/example/abrahamalfredbabu_piamaeobias_satenderyadav_mapd711_project/activities/AddTaskActivity.kt
@@ -0,0 +1,12 @@
+package com.example.abrahamalfredbabu_piamaeobias_satenderyadav_mapd711_project.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.abrahamalfredbabu_piamaeobias_satenderyadav_mapd711_project.R
+
+class AddTaskActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_task)
+    }
+}
